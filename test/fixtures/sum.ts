4962d0bfcf87112e82ef22d6825d27ebc8a2c5d5
Toru Kobayashi <koba0004@gmail.com>
2019-09-15 18:55:23 +0900

    feat: enable new rules(no-import-assign, default-param-last, prefer-regex-literals)
    BREAKING CHANGE: enable no-import-assign, default-param-last, prefer-regex-literals rules

--- test/fixtures/sum.ts
+++ test/fixtures/sum.ts
@@ -1,3 +1,17 @@
+import { mod } from './mod'
+
+mod = 'mod'
+console.log(mod)
+
+function fn(a: any, b = 'b', c: any) {
+  // noop
+}
+fn(1, '2', 3)
+
+const regexp = new RegExp('foo|bar')
+console.log(regexp)
+
 export const sum = (a: number, b: number): number => a + b
 export const bar = (bar: string) => ''
 export const isMatch = (str: string): boolean => /=\d{10}/.test(str);
+
