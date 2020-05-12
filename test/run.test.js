1504ccb3f98e4f5ae640c49780fbf01e271d89fa
Toru Kobayashi <koba0004@gmail.com>
2020-05-13 00:02:43 +0900

    test: use ESLint class instead of CLIEngine

--- test/run.test.js
+++ test/run.test.js
@@ -1,8 +1,8 @@
 const runLint = require('./lib/runLint')
 
 describe('fixtures', () => {
-  it('should match the snapshot', () => {
-    const result = runLint('./index.js', './test/fixtures')
+  it('should match the snapshot', async () => {
+    const result = await runLint('./index.js', './test/fixtures')
     expect(result).toMatchSnapshot()
   })
 })
