e09c9f215bb0c76942af5a13af5144426a088a4c
AtsushiM <at.mizoue@gmail.com>
2022-01-01 09:49:26 +0900

    fix: a11y-icon-button-has-name

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const rulesDirPlugin = require('eslint-plugin-rulesdir')
+
+rulesDirPlugin.RULES_DIR = './rules'
+
 module.exports = {
   extends: [
     './configs/eslint.js',
@@ -5,10 +9,14 @@ module.exports = {
     './configs/react.js',
     './configs/prettier.js',
   ],
+  plugins: ['rulesdir'],
   rules: {
+    '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/no-var-requires': 'off',
     // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-shadow.md
     'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
+
+    // original rules
+    'rulesdir/a11y-icon-button-has-name': 'error',
   },
 }
